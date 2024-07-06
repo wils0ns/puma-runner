c5ff28d4b1b7bac7feb5e53c5931e99e42928a2d
Wilson Santos <wilson@codeminus.org>
2024-07-06 13:30:28 -0500

    add run.sh script

--- run.sh
+++ run.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
 
-PUMA_INPUT_FILE=${1}
+inputFile=${1}
+pumaVersion=${PUMA_VERSION:-1.2.2}
+runnerDataDir=~/.puma-runner/data
 
-PUMA_GIT_REPO=https://github.com/KVD-lab/puma.git
-PUMA_VERSION=1.2.2
-
-
-# Define and create puma-runner's data and cache folders
-PUMA_RUNNER_DATA_DIR=~/.puma-runner/data
-PUMA_RUNNER_CACHE_DIR=~/.puma-runner/cache
-mkdir -p ${PUMA_RUNNER_DATA_DIR}
-mkdir -p ${PUMA_RUNNER_CACHE_DIR}
-
-
-# Clone KVD-lab/puma repository to get access to its 'data_dir'
-git -C ${PUMA_RUNNER_CACHE_DIR} clone ${PUMA_GIT_REPO} repository || true
-# TODO: if repo already exists update local.
-
-
-# Copy input data inside puma-runner's data folder
-cp ${PUMA_INPUT_FILE} ${PUMA_RUNNER_DATA_DIR}/input.fas
+mkdir -p ${runnerDataDir}
+cp ${inputFile} ${runnerDataDir}/input.fas
 
 docker run --rm \
-    -v ${PUMA_RUNNER_DATA_DIR}:/puma-data \
-    -v ${PUMA_RUNNER_CACHE_DIR}:/puma-cache \
-    kvdlab/puma:${PUMA_VERSION} \
-    run_puma.py -i /puma-data/input.fas -o /puma-data/puma-results -d /puma-cache/repository/data_dir/
+    -v ${runnerDataDir}:/puma-data \
+    kvdlab/puma:${pumaVersion} \
+    run_puma.py \
+        -i /puma-data/input.fas \
+        -o /puma-data/puma-results \
+        -d /app/puma/data_dir/
